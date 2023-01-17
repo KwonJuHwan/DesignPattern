fc8d9358150fdc93268ffb602ad3ec328da0b667
JuHwan Kwon <118177454+KwonJuHwan@users.noreply.github.com>
2023-01-17 17:01:40 +0900

    Delete DisplayDecorator.java

--- DisplayDecorator.java
+++
@@ -1,9 +0,0 @@
-package HIghHomework5;
-import javax.swing.*;
-
-public abstract class DisplayDecorator extends Display {
-    DisplayDecorator(Display display, int width, int height) {
-        super(width, height);
-    }
-}
-
