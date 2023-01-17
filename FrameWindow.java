6e978645d820adc58ef42ab08052b90e2523c694
JuHwan Kwon <118177454+KwonJuHwan@users.noreply.github.com>
2023-01-17 17:01:49 +0900

    Delete FrameWindow.java

--- FrameWindow.java
+++
@@ -1,8 +0,0 @@
-package HIghHomework5;
-import javax.swing.*;
-import java.awt.event.WindowListener;
-
-public abstract class FrameWindow {
-
-    public abstract JPanel createPanel(int width, int height);
-}
